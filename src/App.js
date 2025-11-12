52cbb573deb64e3bf841a89e82a9d44b1ea677f3
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2025-11-12 22:50:00 +0200

    Hardcoded catergories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-
-
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div className="catergories-container">
+      <div className="catergory-container">
+        {/* <img></img> */}
+        <div className="catergory-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catergory-container">
+        {/* <img></img> */}
+        <div className="catergory-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catergory-container">
+        {/* <img></img> */}
+        <div className="catergory-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catergory-container">
+        {/* <img></img> */}
+        <div className="catergory-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="catergory-container">
+        {/* <img></img> */}
+        <div className="catergory-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;

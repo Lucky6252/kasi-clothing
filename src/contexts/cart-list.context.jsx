1e2f0604b0cdc161e2e70dfbdefbb8f4cd459dfe
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2025-12-13 02:30:55 +0200

    Adding items to cart part 1

--- src/contexts/cart-list.context.jsx
+++ src/contexts/cart-list.context.jsx
@@ -3,11 +3,35 @@ import { createContext, useState } from "react";
 export const CartContext = createContext({
     visibility: false,
     setVisibility: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
 });
+
+const addCartItem = (cartItems, productToAdd) => {
+
+    const existingProduct = cartItems.find(item => item.id === productToAdd.id);
+
+    if(existingProduct){
+       return cartItems.map(item => item.id === productToAdd.id ? {...item, quantity: item.quantity + 1}: item);
+        
+    }
+    else {
+        return [...cartItems, {...productToAdd, quantity: 1}]
+    }
+
+}
 
 export const CartProvider = ({children}) => {
     const [visibility, setVisibility] = useState(false);
-    const value = {visibility, setVisibility};
+    const [ cartItems, setCardItems] = useState([])
+
+    const addItemToCart = (productToAdd) => {
+        setCardItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = {visibility, setVisibility, addItemToCart, cartItems};
+
+    
 
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>

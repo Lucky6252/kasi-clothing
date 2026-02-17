a45212257dc908751cb85923aa84004d8bb722b4
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2026-02-17 20:13:55 +0200

    Adding redux and creating store, user reducer,types and actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { onAuthStateChangedListener, createUserDocFromAuth } from "./utils/firebase/firebase.utils";
 import Home from "./routes/home/home.component";
 import NavigationBar from "./routes/navigation-bar/navigation-bar.component";
 import Authentication from "./routes/authentication/authentication.component";
 import { Routes, Route } from "react-router-dom";
 import Shop from "./routes/shop/shop.component";
 import CheckOut from "./routes/checkout/checkout.component";
+import { setCurrentUser } from "./store/user/user.action";
 
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+          const unsubscribe = onAuthStateChangedListener((user)=>{
+              if(user){
+                  createUserDocFromAuth(user);
+              }
+              dispatch(setCurrentUser(user));
+          });
+  
+          return unsubscribe;
+      }, []);
+
+
   return (
     <Routes>
       <Route path="/" element={<NavigationBar />}>

3d6039c6f176ab0e35faa7aa5b26b63840f9ae73
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2026-02-18 12:38:23 +0200

    Converting the App to use redux from context

--- src/routes/navigation-bar/navigation-bar.component.jsx
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -1,9 +1,10 @@
 import { Fragment } from "react/jsx-runtime";
 import { Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import { useContext } from "react";
-import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown-component";
@@ -12,7 +13,7 @@ import { CartContext } from "../../contexts/cart-list.context";
 import { Navigation, LogoContainer, NavLink, NavLinks } from "./navigation-bar.styles";
 
 const NavigationBar = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { visibility, setVisibility } = useContext(CartContext);
 
   return (

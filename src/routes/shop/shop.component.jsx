2da4fdecd5f54080c0c35a6b7ec376af3c638559
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2026-02-20 23:56:08 +0200

    Adding redux thunk and applying async redux thunk within categories action

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -6,20 +6,13 @@ import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { setCategories } from "../../store/categories/categories.action";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-
-      console.log(categoriesArray);
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+      dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,10 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-
+import { thunk } from "redux-thunk";
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(
   Boolean,
 );
 
